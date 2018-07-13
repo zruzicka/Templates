5b62d32942f1cded874e515aecbc5187b8c3f895
Zbynek Ruzicka <zruzicka2@gmail.com>
2018-07-13 07:58:37 +0200

    (Templates/java) Added initial Liquibase related demo. Update executed via liquibase-maven-plugin. Initial changelog resources.

---
+++ java/Liquibase/src/main/java/cz/zr/liquibase/App.java
@@ -0,0 +1,12 @@
+package cz.zr.liquibase;
+
+/**
+ * Initial template class.
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Template executed.");
+	}
+
+}
